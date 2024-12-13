07e69ad7377382df93895f993a90ce772a0b4909
Ramakrishna Kumbar <ramlikestocode@gmail.com>
2024-12-13 23:19:58 +0530

    escape sequence character

---
+++ Chapter1/04escapeSequence.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    int a;
+    char ch = '1';
+    ch = '1';
+    ch = '\n'; //this represents new line
+    printf("Hey Im line no 8 \n over and out \\n \t using \\t to add some space here");
+    // "\t" puts and space, \" used to put double quote, \\ will print a single backslash,
+    return 0;
+}
