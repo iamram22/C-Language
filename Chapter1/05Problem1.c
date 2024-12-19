57eb07cfeedfa7442d1eec5da54eb63b907a5ac2
Ramakrishna Kumbar <ramlikestocode@gmail.com>
2024-12-19 23:56:16 +0530

    some Maths

---
+++ Chapter1/05Problem1.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main() {
+    int r = 6;
+    int height = 8;
+    printf("The area of circle with radious %d, is %f\n", r, 3.14*r*r);
+    printf("volume of the cylinder with radius %d, and height %d is %f", r, height, 3.14*r*r*height);
+}
---
+++ Chapter1/06Problem2.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main() {
+    float c;
+    float f;
+    f = (9/5)*c + 32;
+    printf("the value in Fahrenheit is %f", f);
+}
