0ecbfeeb74fb83cb2c291aa98db8816aaa9f210c
Ram K <ramlikestocode@gmail.com>
2025-07-24 22:49:06 +0530

    area

--- IIT-Madras/W3/GrPA5.c
+++ IIT-Madras/W3/GrPA5.c
@@ -10,3 +10,19 @@ int main() {
 printf("%d %d",a,b); 
     return 0;
 }
+
+// GrPA5
+
+#include <stdio.h>
+#include <math.h>
+int main() {
+    double a, b, c;
+    scanf("%lf %lf %lf", &a, &b, &c);
+    // Write solution code below
+
+
+double s = (a + b + c) / 2.0;  
+    double area = sqrt(s * (s - a) * (s - b) * (s - c)); 
+    printf("%.2lf", area);
+    return 0;
+}
