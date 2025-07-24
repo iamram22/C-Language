c99808113b83f136a124226a7a36ad1de37086fa
Ram K <ramlikestocode@gmail.com>
2025-07-24 22:50:12 +0530

    Pizza cost

--- IIT-Madras/W3/GrPA6.c
+++ IIT-Madras/W3/GrPA6.c
@@ -20,3 +20,30 @@ int main()
     printf("%.0lf", monthlyPayment);
     return 0;
 }
+
+// GrPA6
+
+#include <stdio.h>
+#include <math.h>
+int main() 
+{
+    // Initialization of Variables
+    double diameter, costPerSqInch, costperTopping, totalCost;
+    double pi = 3.14;
+    int numToppings;
+	
+    // Reading Input
+    scanf("%lf", &diameter);
+    scanf("%lf", &costPerSqInch);
+    scanf("%d", &numToppings);
+    scanf("%lf", &costperTopping);  
+    // Write code below to calculate the total cost and store the result in totalCost variable
+double radius = diameter / 2.0;  
+    double area = pi * radius * radius;  
+    totalCost = area * costPerSqInch + numToppings * costperTopping;  
+  
+    // Print the total cost
+    printf("%.2lf", totalCost);
+    return 0;
+}
+
