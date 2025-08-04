80c7ab7b0d166111c5a96130b3d2d8d35f73965d
Ram K <ramlikestocode@gmail.com>
2025-08-04 23:18:06 +0530

    min_max_swap

--- IIT-Madras/W6/GrPA2.c
+++ IIT-Madras/W6/GrPA2.c
@@ -1,26 +1,32 @@
-// To complete the program, write a function swap that takes two integers pointer a and b and swaps the values of a and b.
+// Write a function min_max_swap that accepts five addresses which contain five distinct integer elements. The function swap the location of minimum and maximum elements.
 
-// Note: The function does not return anything.
-
-#include <stdio.h>
+#include<stdio.h>
 //Write function below
-void swap(int *a, int *b)
-{
-    int temp = *a;
-    *a = *b;
-    *b = temp;
+void min_max_swap(int *p1, int *p2, int *p3, int *p4, int *p5) {
+    int *arr[5] = { p1, p2, p3, p4, p5 };
+    int *min_ptr = arr[0];
+    int *max_ptr = arr[0];
+    
+    // Find pointers to min and max values
+    for (int i = 1; i < 5; i++) {
+        if (*arr[i] < *min_ptr) {
+            min_ptr = arr[i];
+        }
+        if (*arr[i] > *max_ptr) {
+            max_ptr = arr[i];
+        }
+    }
+    
+    // Swap the values at min and max locations
+    int temp = *min_ptr;
+    *min_ptr = *max_ptr;
+    *max_ptr = temp;
 }
-int main() 
+
+int main()
 {
-    int a, b;
-    scanf("%d", &a);
-    scanf("%d", &b);
-    
-    swap(&a, &b);
-    
-    printf("%d", a);
-    printf("\n");
-    printf("%d", b);
-    
-    return 0;
+    int a,b,c,d,e;
+    scanf("%d%d%d%d%d",&a,&b,&c,&d,&e);
+    min_max_swap(&a,&b,&c,&d,&e);
+    printf("%d %d %d %d %d",a,b,c,d,e);
 }
