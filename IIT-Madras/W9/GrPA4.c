2c0cdc1fa9e77c09953001b24dbebbcdc681fd16
Ram K <ramlikestocode@gmail.com>
2025-08-21 20:55:43 +0530

    diagonalSum

---
+++ IIT-Madras/W9/GrPA4.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+//Write function below
+int diagonalSum(int N, int matrix[N][N]) {
+    int sum = 0;
+    for (int i = 0; i < N; i++) {
+        sum += matrix[i][i];
+    }
+    return sum;
+}
+
+
+int main() 
+{
+int N;
+scanf("%d", &N);
+int matrix[N][N];
+for (int i = 0; i < N; i++)
+{
+for (int j = 0; j < N; j++)
+{
+scanf("%d", &matrix[i][j]);
+}
+}
+printf("%d\n",diagonalSum(N,matrix));
+return 0;
+}
